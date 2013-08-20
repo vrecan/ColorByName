c70a01c2ed4a6cba27a64204338c7111411b8a79
Ben Aldrich <vrecan@gmail.com>
2013-08-20 14:24:20 -0600

    Update colorByName.php

--- colorByName.php
+++ colorByName.php
@@ -22,7 +22,9 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 THE SOFTWARE.
  */
+<?php
 
 function getColor($string) {
   return substr(strrev(sha1($string)), 0, 6);
 } 
+?>
